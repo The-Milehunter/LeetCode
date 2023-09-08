dd770e02f576355692f271daf69e2deb7e3f3d95
The-Milehunter <kavinsp77@gmail.com>
2023-09-08 22:01:56 +0530

    Two sum

---
+++ src/com/kavin/arrays/Main.java
@@ -0,0 +1,7 @@
+package com.kavin.arrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+}
--- src/com/kavin/arrays/TwoSum_1.java
+++ src/com/kavin/arrays/TwoSum_1.java
@@ -1,21 +1,31 @@
 package com.kavin.arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-@SuppressWarnings("unused")
+
 public class TwoSum_1 {
 
-    public int[] twoSum(int[] nums, int target) {
+    public static void main (String[] args) {
+        TwoSum_1 twoSum = new TwoSum_1 ();
+        int[] input = {2, 3, 5, 6, 11, -1, 9};
+        int[] resultIndexes = twoSum.twoSum (input, 10);
+        System.out.println (Arrays.toString (resultIndexes));
+    }
+
+    public int[] twoSum (int[] nums, int target) {
         // key -> complement(difference from target) : value -> index
-        Map <Integer,Integer> cache = new HashMap<>();
+        Map<Integer, Integer> cache = new HashMap<> ();
         for (int i = 0; i < nums.length; i++) {
-            if (cache.get(nums[i]) != null)
-                return new int[] {cache.get(nums[i]),i};
-            cache.put(target-nums[i],i);
+            if (cache.get (nums[i]) != null)
+                return new int[]{cache.get (nums[i]), i};
+            cache.put (target - nums[i], i);
         }
         return nums;
     }
-    public int[] twoSumSlow(int[] nums, int target) {
+
+    @SuppressWarnings("unused")
+    public int[] twoSumSlow (int[] nums, int target) {
         for (int i = 0; i < nums.length; i++)
             for (int j = i + 1; j < nums.length; j++)
                 if (nums[i] + nums[j] == target)
